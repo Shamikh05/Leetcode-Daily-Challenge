fc2135626bbb52ebbf14e3fbe83277aa7b72cde9
Shamikh05 <shamiikhullah.akram18@gmail.com>
2022-12-08 02:17:16 +0530

    Using BST properties. O(N) Time Complexity. O(N) Space Complexity (Recursive Stack).

---
+++ December-2022/Day-7/range-sum-of-bst.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(root == NULL) {
+            return 0;
+        }
+
+        // Get the current node value.
+        int currVal = root->val;
+        // If the node value is within range, take the value into sum else leave it.
+        int sum = (currVal >= low && currVal <= high) ? currVal : 0;        
+
+        // If the current node >= low, go to left to find the node with lesser value. 
+        if(currVal >= low) {
+            sum += rangeSumBST(root->left, low, high);    
+        }
+
+        // If the current node <= high, go to right to find the node with greater value.
+        if(currVal <= high) {
+            sum += rangeSumBST(root->right, low, high);    
+        }
+        
+        // Return the final sum.
+        return sum;
+    }
+};
